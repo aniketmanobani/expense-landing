52d3d5195ae64dddebc5a41864f64111bd1a94a4
Aniket <aniket.fleet@gmail.com>
2023-10-29 11:56:35 +0530

    ok

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,51 +1,62 @@
-import React from 'react'
-import './style.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
 function Homepage() {
-    return (
-        <>
-            {/* MENU */}
-            <header className="menu-bg">
-                <div className="menu">
-                    <div>
-                        <img src="/img/mobile-app-logo.png" alt="Mobile App logo" />
-                    </div>
-                    <nav className="menu-nav">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><Link to="/p/privacy-policy">Privacy Policy</Link></li>
-                            <li><a href="#">About</a></li>
-                            <li className="btn-menu"><a href="#">Download</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+  return (
+    <>
+      {/* MENU */}
+      <header className="menu-bg">
+        <div className="menu">
+          <div>
+            <img src="/img/mobile-app-logo.png" alt="Mobile App logo" />
+          </div>
+          <nav className="menu-nav">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <Link to="/p/privacy-policy">Privacy Policy</Link>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li className="btn-menu">
+                <a href="#">Download</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
 
-            {/* INTRO */}
-            <section className="intro-bg">
-                <div className="intro">
-                    <div>
-                        <h1>D2 Wallet <br /><span>Budgets & Expense Tracker App</span></h1>
-                        <p>Easy and Smart way to manage all Expenses</p>
-                        <a href="#">Download</a>
-                    </div>
-                    <div className="intro-img">
-                        <img src="img/mocukup.png" alt="" />
-                    </div>
-                </div>
-            </section>
+      {/* INTRO */}
+      <section className="intro-bg">
+        <div className="intro">
+          <div>
+            <h1>
+              CashTrackr <br />
+              <span>Budgets & Expense Tracker App</span>
+            </h1>
+            <p>Easy and Smart way to manage all Expenses</p>
+            <a href="#">Download</a>
+          </div>
+          <div className="intro-img">
+            <img src="img/mocukup.png" alt="" />
+          </div>
+        </div>
+      </section>
 
-            {/* INFO */}
-            <section className="info-bg">
-                <div className="info">
-                    <h3>Get in touch by</h3>
-                    <img src="/img/facebook-mesenger-icon.png" alt="Mesenger Logo" />
-                    <p>Smart Expense Tracker app on playstore</p>
-                </div>
-            </section>
+      {/* INFO */}
+      <section className="info-bg">
+        <div className="info">
+          <h3>Get in touch by</h3>
+          <img src="/img/facebook-mesenger-icon.png" alt="Mesenger Logo" />
+          <p>Smart Expense Tracker app on playstore</p>
+        </div>
+      </section>
 
-            {/* FEATURE */}
-            {/* <section className="feature">
+      {/* FEATURE */}
+      {/* <section className="feature">
                 <div className="feature-item">
                     <h4>Easy to Edit</h4>
                     <img src="/img/easy-to-edit.png" alt="Easy To Edit Logo" />
@@ -63,8 +74,8 @@ function Homepage() {
                 </div>
             </section> */}
 
-            {/* ABOUT1 */}
-            {/* <section className="about-bg">
+      {/* ABOUT1 */}
+      {/* <section className="about-bg">
                 <div className="about">
                     <div className="about-img">
                         <img src="/img/Mockup2.png" alt="Mockup2" />
@@ -77,8 +88,8 @@ function Homepage() {
                 </div>
             </section> */}
 
-            {/* ABOUT2 */}
-            {/* <section className="about-bg2">
+      {/* ABOUT2 */}
+      {/* <section className="about-bg2">
                 <div className="about">
                     <div className="about-info2">
                         <h4>Track Shipments</h4>
@@ -91,8 +102,8 @@ function Homepage() {
                 </div>
             </section> */}
 
-            {/* REDE-SOCIAL */}
-            {/* <section className="rede-social-bg">
+      {/* REDE-SOCIAL */}
+      {/* <section className="rede-social-bg">
                 <div className="rede-social">
                     <img src="/img/instagram-icon.png" alt="Instagram Logo" />
                     <img src="/img/facebook-icon.png" alt="Instagram Logo" />
@@ -101,37 +112,55 @@ function Homepage() {
                 </div>
             </section> */}
 
-            {/* FOOTER */}
-            <footer className="footer-bg">
-                <div className="footer">
-                    <div className="menu">
-                        <ul>
-                            <p>MENU</p>
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">Showcase</a></li>
-                            <li><a href="#">Download</a></li>
-                        </ul>
-                    </div>
-                    <div className="menu">
-                        <ul>
-                            <p>ABOUT</p>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Journal</a></li>
-                        </ul>
-                    </div>
-                    <div className="menu">
-                        <ul>
-                            <p>PANOPLY</p>
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">Showcase</a></li>
-                            <li><a href="#">Download</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
+      {/* FOOTER */}
+      <footer className="footer-bg">
+        <div className="footer">
+          <div className="menu">
+            <ul>
+              <p>MENU</p>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Showcase</a>
+              </li>
+              <li>
+                <a href="#">Download</a>
+              </li>
+            </ul>
+          </div>
+          <div className="menu">
+            <ul>
+              <p>ABOUT</p>
+              <li>
+                <a href="#">Company</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Journal</a>
+              </li>
+            </ul>
+          </div>
+          <div className="menu">
+            <ul>
+              <p>PANOPLY</p>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Showcase</a>
+              </li>
+              <li>
+                <a href="#">Download</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Homepage
+export default Homepage;
--- src/pages/PrivacyPolicy/index.jsx
+++ src/pages/PrivacyPolicy/index.jsx
@@ -1,52 +1,130 @@
-import React from 'react'
+import React from "react";
 
 function Privacy() {
-    return (
-        <>
-            <h1>Privacy Policy for D2 Wallet</h1>
-            <p>Effective Date: 10-06-2023</p>
-            <p>Thank you for choosing D2 Wallet ("the App"). This Privacy Policy outlines how we collect, use, disclose, and protect your personal information when you use our expense tracker app. By using the App, you agree to the terms and practices described in this Privacy Policy.</p>
-            <h2>1. Information We Collect:</h2>
-            <ul>
-                <li>Personal Information: When you create an account with D2 Wallet, we may collect personal information such as your name, email address, and any other information you provide during the registration process or while using certain features of the App.</li>
-                <li>Financial Information: The App may require you to provide financial information, such as bank account details or credit card information, solely for the purpose of facilitating fund transfers or generating financial reports. We treat your financial information with the utmost confidentiality and employ industry-standard security measures to protect it.</li>
-                <li>Usage Information: We may collect non-personal information about your use of the App, including device information, operating system, and app usage statistics. This information helps us improve the App's performance, diagnose technical issues, and provide a better user experience.</li>
-            </ul>
-            <h2>2. Use of Information:</h2>
-            <ul>
-                <li>We use your personal information to create and maintain your D2 Wallet account, provide you with access to the App's features, and personalize your experience within the App.</li>
-                <li>Financial information is securely stored and used solely for the purpose of facilitating fund transfers or generating financial reports as per your instructions.</li>
-                <li>We may use the usage information collected to analyze trends, enhance the App's functionality, improve our services, and optimize user experience.</li>
-            </ul>
-            <h2>3. Disclosure of Information:</h2>
-            <ul>
-                <li>We do not sell, rent, or lease your personal information to third parties. However, we may disclose your information to trusted service providers who assist us in operating the App or providing services to you, subject to strict confidentiality obligations.</li>
-                <li>We may disclose your personal information if required by law or in good faith belief that such disclosure is necessary to comply with legal obligations, protect our rights or interests, investigate fraud, or enforce our Terms of Service.</li>
-            </ul>
-            <h2>4. Data Security:</h2>
-            <ul>
-                <li>We implement industry-standard security measures to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</li>
-                <li>It is essential that you maintain the confidentiality of your D2 Wallet account login credentials and report any unauthorized access to your account immediately.</li>
-            </ul>
-            <h2>5. Third-Party Links and Services:</h2>
-            <ul>
-                <li>The App may contain links to third-party websites or services. We are not responsible for the privacy practices or content of these third parties. We recommend reviewing their respective privacy policies before providing any personal information.</li>
-            </ul>
-            <h2>6. Children's Privacy:</h2>
-            <ul>
-                <li>The App is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children. If you believe we have inadvertently collected personal information from a child, please contact us to have it removed.</li>
-            </ul>
-            <h2>7. Changes to this Privacy Policy:</h2>
-            <ul>
-                <li>We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any significant changes by posting the updated Privacy Policy within the App. Your continued use of the App after any modifications to the Privacy Policy will signify your acceptance of the updated terms.</li>
-            </ul>
-            <h2>8. Contact Us:</h2>
-            <ul>
-                <li>If you have any questions, concerns, or requests regarding this Privacy Policy or the privacy practices of D2 Wallet, please contact us at <a href="mailto:makeinsoft@gmail.com">makeinsoft@gmail.com</a>.</li>
-            </ul>
-            <p>By using D2 Wallet, you acknowledge that you have read and understood this Privacy Policy and consent to the collection, use, and disclosure of your personal information as described herein.</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Privacy Policy for CashTrackr</h1>
+      <p>Effective Date: 10-06-2023</p>
+      <p>
+        Thank you for choosing CashTrackr ("the App"). This Privacy Policy
+        outlines how we collect, use, disclose, and protect your personal
+        information when you use our expense tracker app. By using the App, you
+        agree to the terms and practices described in this Privacy Policy.
+      </p>
+      <h2>1. Information We Collect:</h2>
+      <ul>
+        <li>
+          Personal Information: When you create an account with CashTrackr, we
+          may collect personal information such as your name, email address, and
+          any other information you provide during the registration process or
+          while using certain features of the App.
+        </li>
+        <li>
+          Financial Information: The App may require you to provide financial
+          information, such as bank account details or credit card information,
+          solely for the purpose of facilitating fund transfers or generating
+          financial reports. We treat your financial information with the utmost
+          confidentiality and employ industry-standard security measures to
+          protect it.
+        </li>
+        <li>
+          Usage Information: We may collect non-personal information about your
+          use of the App, including device information, operating system, and
+          app usage statistics. This information helps us improve the App's
+          performance, diagnose technical issues, and provide a better user
+          experience.
+        </li>
+      </ul>
+      <h2>2. Use of Information:</h2>
+      <ul>
+        <li>
+          We use your personal information to create and maintain your
+          CashTrackr account, provide you with access to the App's features, and
+          personalize your experience within the App.
+        </li>
+        <li>
+          Financial information is securely stored and used solely for the
+          purpose of facilitating fund transfers or generating financial reports
+          as per your instructions.
+        </li>
+        <li>
+          We may use the usage information collected to analyze trends, enhance
+          the App's functionality, improve our services, and optimize user
+          experience.
+        </li>
+      </ul>
+      <h2>3. Disclosure of Information:</h2>
+      <ul>
+        <li>
+          We do not sell, rent, or lease your personal information to third
+          parties. However, we may disclose your information to trusted service
+          providers who assist us in operating the App or providing services to
+          you, subject to strict confidentiality obligations.
+        </li>
+        <li>
+          We may disclose your personal information if required by law or in
+          good faith belief that such disclosure is necessary to comply with
+          legal obligations, protect our rights or interests, investigate fraud,
+          or enforce our Terms of Service.
+        </li>
+      </ul>
+      <h2>4. Data Security:</h2>
+      <ul>
+        <li>
+          We implement industry-standard security measures to protect your
+          personal information from unauthorized access, use, or disclosure.
+          However, no method of transmission over the internet or electronic
+          storage is 100% secure, and we cannot guarantee absolute security.
+        </li>
+        <li>
+          It is essential that you maintain the confidentiality of your
+          CashTrackr account login credentials and report any unauthorized
+          access to your account immediately.
+        </li>
+      </ul>
+      <h2>5. Third-Party Links and Services:</h2>
+      <ul>
+        <li>
+          The App may contain links to third-party websites or services. We are
+          not responsible for the privacy practices or content of these third
+          parties. We recommend reviewing their respective privacy policies
+          before providing any personal information.
+        </li>
+      </ul>
+      <h2>6. Children's Privacy:</h2>
+      <ul>
+        <li>
+          The App is not intended for use by individuals under the age of 18. We
+          do not knowingly collect personal information from children. If you
+          believe we have inadvertently collected personal information from a
+          child, please contact us to have it removed.
+        </li>
+      </ul>
+      <h2>7. Changes to this Privacy Policy:</h2>
+      <ul>
+        <li>
+          We reserve the right to update or modify this Privacy Policy at any
+          time. We will notify you of any significant changes by posting the
+          updated Privacy Policy within the App. Your continued use of the App
+          after any modifications to the Privacy Policy will signify your
+          acceptance of the updated terms.
+        </li>
+      </ul>
+      <h2>8. Contact Us:</h2>
+      <ul>
+        <li>
+          If you have any questions, concerns, or requests regarding this
+          Privacy Policy or the privacy practices of CashTrackr, please contact
+          us at <a href="mailto:makeinsoft@gmail.com">makeinsoft@gmail.com</a>.
+        </li>
+      </ul>
+      <p>
+        By using CashTrackr, you acknowledge that you have read and understood
+        this Privacy Policy and consent to the collection, use, and disclosure
+        of your personal information as described herein.
+      </p>
+    </>
+  );
 }
 
-export default Privacy
+export default Privacy;
